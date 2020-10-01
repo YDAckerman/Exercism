106d8f75b50c2b2c11a8b1c8dcf603734a867d77
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2020-10-01 13:59:14 -0700

    another called grains

---
+++ python/grains/grains.py
@@ -0,0 +1,8 @@
+def square(number):
+    if number < 1 or number > 64:
+        raise ValueError("Number must be between 1 and 64 (inclusive)")
+    return 2 ** (number - 1)
+
+
+def total():
+    return 18446744073709551615
