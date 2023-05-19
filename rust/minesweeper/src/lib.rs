6dee75c00685f32484513b7a01eaab2e7338f09a
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2023-05-19 13:38:33 -0400

    all tests passing

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,61 +1,66 @@
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
 
-    let board: Vec<&[u8]> = minefield.iter()
-        .map(|x| x.as_bytes())
-        .collect();
-
-    board.iter()
+    minefield.iter()
         .enumerate()
         .map(|(i, row)| 
-             row
+             row.as_bytes()
              .iter()
              .enumerate()
              .map(|(j, b)| match b {
                  42 => String::from('*'),
-                 _ => count_mines_by([i, j], &board),
+                 _ => count_to_string(
+                     count_mines_by((i, j), minefield)
+                 ),
              })
              .collect::<Vec<String>>().join("")
         ).collect()
     
 }
 
-fn count_mines_by(point: [usize; 2], board: &Vec<&[u8]>) -> String {
-
-    let dims: [usize; 2] = [board.len(), board[0].len()];
-    let mine_count: usize = get_nbs(&point, &dims)
-        .iter()
-        .map(|nb| is_mine(*nb, board))
-        .sum();
-
-    match mine_count {
+fn count_to_string(count: usize) -> String {
+    match count {
         0 => String::from(" "),
-        _ => mine_count.to_string(),
+        _ => count.to_string(),
     }
-    
-    
 }
 
-fn get_nbs(p: &[usize; 2], dims: &[usize; 2]) -> Vec<[usize; 2]> {
+fn count_mines_by(point: (usize, usize), board: &[&str]) -> usize {
+
+    let dims: (usize, usize) = (board.len(), board[0].len());
+    get_nbs(point, dims)
+        .iter()
+        .map(|nb| is_mine(*nb, board))
+        .sum()
+
+}
+
+fn get_nbs((x, y): (usize, usize), (m, n): (usize, usize)) -> Vec<(usize, usize)> {
  
-    let nbs = vec![[-1, -1], [-1, 0], [0, -1], [-1, 1], [1, 1],
-                   [1, 0], [0, 1], [1, -1]];
+    let nbs: Vec<(i32, i32)> = vec![(-1, -1), (-1, 0), (0, -1),
+                                    (-1, 1), (1, 1), (1, 0),
+                                    (0, 1), (1, -1)];
     nbs.into_iter()
-        .map(|v| vec![v[0] + (p[0] as i32), v[1] + (p[1] as i32)])
-        .filter(|v| (v[0] >= 0) & (v[0] < (dims[0] as i32)) 
-                & (v[1] >= 0) & (v[1] < (dims[1] as i32)))
-        .map(|v| [v[0] as usize, v[1] as usize])
+        .map(|(i, j)| (i + (x as i32), j + (y as i32)))
+        .filter(|(i, j)| (i >= &0) & (i < &(m as i32)) 
+                & (j >= &0) & (j < &(n as i32)))
+        .map(|(i, j)| (i as usize, j as usize))
         .collect::<Vec<_>>()
                       
 }
  
-fn is_mine(nb: [usize; 2], board: &Vec<&[u8]>) -> usize {
+fn is_mine((x,y): (usize, usize), board: &[&str]) -> usize {
 
-    let board_val: u8 = board[nb[0]][ nb[1]];
-     match board_val {
-         42 => 1,
-         _ => 0,
-     }
+    let board_val: u8 = board[x].as_bytes()[y];
+    match board_val {
+        42 => 1,
+        _ => 0,
+    }
      
 }
+
+trait CountString {
+    fn to_count_string(&self) -> String;
+}
+
 
